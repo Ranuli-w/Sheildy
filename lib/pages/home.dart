3fd0834bb771ee01119bddbf43e3f8b628802496
kethaka <hirunkethaka@gmail.com>
2024-03-09 11:13:21 +0530

    change the home appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,37 @@
-import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const  HomeScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shieldy/utils/colors.dart';
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
 
   @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
-      ),
+    return Scaffold(
+      appBar: AppBar(
+        
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {
+              // Implement the action when the profile icon is pressed
+            },
+          ),Spacer(),
+          IconButton(
+            icon: const Icon(Icons.notifications_none),
+            onPressed: () {
+              // Implement the action when the notification icon is pressed
+            },
+          ),
+        ],backgroundColor: mobileBackgroundColor
+        
+      ),backgroundColor: mobileBackgroundColor,
     );
   }
 }
+
