abed8634be9b9b6e1f710252785b8383822c9aa3
wimukthideshan <wimukthideshan@gmail.com>
2024-03-14 00:50:14 +0530

    commit before reverting

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shieldy/pages/account_screen.dart';
 import 'package:shieldy/pages/add_post_page.dart';
 import 'package:shieldy/pages/auth_page.dart';
 import 'package:shieldy/pages/welcome_page.dart'; // Import the WelcomePage class
 import 'package:firebase_core/firebase_core.dart';
-import 'package:shieldy/responsive/mobileScreen.dart';
 import 'package:shieldy/utils/colors.dart';
 import 'firebase_options.dart';
 
