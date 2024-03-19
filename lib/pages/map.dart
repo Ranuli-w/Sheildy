15393bf571513597e9b0756fc84d547ce16d30c1
Kavindu Palihakkara <148669416+Kavindu809git@users.noreply.github.com>
2024-03-19 17:05:17 +0530

    heat map implimentation // failed

---
+++ lib/pages/map.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+
+class map extends StatefulWidget {
+  const map({super.key});
+
+  @override
+  State<map> createState() => _mapState();
+}
+
+class _mapState extends State<map> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
