1749898ea8b98d4fe4abc7b145bc0a855a9edc87
wimukthideshan <wimukthideshan@gmail.com>
2024-03-20 18:29:40 +0530

    fixed the issues with the sign Out method

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,17 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shieldy/pages/login_screen.dart';
+import 'package:shieldy/resources/auth_method.dart';
 import 'package:shieldy/utils/colors.dart';
 import 'package:shieldy/widgets/HOmemain_container.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+  Homepage({super.key});
 
-  //Sign user out method
-  void signUserOut() async {
-    await FirebaseAuth.instance.signOut();
+  // Create an instance of AuthMethods
+  final AuthMethods _authMethods = AuthMethods();
+
+
+  // Method to show loading dialog
+  void _showLoadingDialog(BuildContext context) {
+    print("Showing loading dialog");
+    showDialog(
+      context: context,
+      barrierDismissible: false, // Prevent user from dismissing the dialog
+      builder: (context) {
+        return Dialog(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircularProgressIndicator(),
+                SizedBox(height: 16),
+                Text('Signing out...'),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +59,19 @@ class Homepage extends StatelessWidget {
 
           //Sign out button
           IconButton(
-            onPressed: signUserOut,
+            onPressed: () async {
+              // Show loading dialog
+              _showLoadingDialog(context);
+              await Future.delayed(Duration(seconds: 2));
+              // Call sign out method
+              await _authMethods.signOut();
+              // Close the loading dialog
+              Navigator.of(context).pop();
+              // Navigate to login screen
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+            },
             icon: const Icon(Icons.logout),
           ),
         ],
