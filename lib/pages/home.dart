47d256fe8dc3e57f1d0f6f48572681d4555bece1
kethaka <hirunkethaka@gmail.com>
2024-03-20 14:10:22 +0530

    create the home page with get methods

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shieldy/utils/colors.dart';
 import 'package:shieldy/widgets/HOmemain_container.dart';
-
-
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -17,34 +16,50 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        backgroundColor: mobileBackgroundColor, // Set background color here
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {
+              // Implement the action when the profile icon is pressed
+            },
+          ),
+          Spacer(),
 
+          IconButton(
+            icon: const Icon(Icons.notifications_none),
+            onPressed: () {
+              // Implement the action when the notification icon is pressed
+            },
+          ),
 
-  backgroundColor: mobileBackgroundColor, // Set background color here
-  actions: [
-    IconButton(
-      icon: const Icon(Icons.account_circle),
-      onPressed: () {
-        // Implement the action when the profile icon is pressed
-      },
-    ),
-    Spacer(),
-
-    IconButton(
-      icon: const Icon(Icons.notifications_none),
-      onPressed: () {
-        // Implement the action when the notification icon is pressed
-      },
-    ),
-
-    //Sign out button
-    IconButton(
-      onPressed: signUserOut,
-      icon: const Icon(Icons.logout),
-    ),
-  ],
+          //Sign out button
+          IconButton(
+            onPressed: signUserOut,
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
-      body:   const FeedContainer(),
-);
-}
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('Posts')
+            .snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => FeedContainer(
+              snap: snapshot.data!.docs[index].data(),
+
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
